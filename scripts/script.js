ee54c21346adb23d9ef59cc22c6919abafeeb88d
Benedita1906 <benedita.silvva0@gmail.com>
2026-06-29 16:39:37 +0100

    Primeira versão

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-console.log("Site iniciado!");
+const botao = document.getElementById("btnPesquisar");
+
+botao.addEventListener("click", function () {
+
+    const cidade = document
+        .getElementById("cidade")
+        .value
+        .toLowerCase();
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card){
+
+        const local = card.dataset.cidade.toLowerCase();
+
+        if(local.includes(cidade) || cidade === ""){
+            card.style.display = "block";
+        }else{
+            card.style.display = "none";
+        }
+
+    });
+
+});
